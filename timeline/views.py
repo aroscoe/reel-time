93372d175b2c0f4597720ac77ba237c89886674b
Anthony Roscoe <me@anthonyroscoe.com>
2010-06-25 00:19:33 -0400

    added ordering when pulling all movies

--- timeline/views.py
+++ timeline/views.py
@@ -3,6 +3,6 @@ from django.views.generic.simple import direct_to_template
 from timeline.models import Movie
 
 def movies(request):
-    movies = Movie.objects.all()
+    movies = Movie.objects.all().order_by('-date_watched')
     return direct_to_template(request, 'base.html', {'movies': movies})
         
